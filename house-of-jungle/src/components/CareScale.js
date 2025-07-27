9085cb1a1e730a5adb993705e64b92deda0c11da
Icksan <icksanmambote@gmail.com>
2025-07-27 14:49:50 +0100

    Update CareScale.js

--- house-of-jungle/src/components/CareScale.js
+++ house-of-jungle/src/components/CareScale.js
@@ -1,15 +1,16 @@
-function CareScale({ scaleValue, careType}) {
-    const range = [1, 2, 3];
-    const scaleType = careType === 'light' ? '☀️' : '💧'
+function CareScale({ scaleValue, careType }) {
+	const range = [1, 2, 3]
+	const scaleType = careType === 'light' ? '☀️' : '💧'
 
-    return (
-        <div className ='lmj-care-scale'>
-            {range.map((rangeElem )=> 
-                scaleValue >= rangeElem ? (
-                    <span key={rangeElem.toSteing()} >{scaleType}</span>
-                ) : null)}
-        </div>
-    )
+	return (
+		<div>
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+		</div>
+	)
 }
 
 export default CareScale
